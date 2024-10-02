e3012887e722d28b6bdf8fdc30ef146aa20ce6b8
Blake Tolmie <166079605+blaketolmie@users.noreply.github.com>
2024-10-02 22:21:50 +1300

    uploaded v1 of LFR.c, readme was updated
    Updated LFR.c
    Updated REAME.md
    
    LFR.c still quite buggy / needs more work and refining

--- LFR.c
+++ LFR.c
@@ -1,8 +1,100 @@
 // define pins used on microcontroller
 
-// reading the sensors
+// reading the sensors (based on hardware - ADC convert 8-bit number from 0 to 255)
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>     // not used in this code yet
 
-// motor speeds
+#define F_CPU 9216000UL			// Updated clock frequency (9.216 MHz)
+#define SENSOR_COUNT 5
 
-// speed control - PID?
+// PID constants
+#define Kp 2						// Proportional
+#define Ki 1						// Integral
+#define Kd 1						// Derivative
 
+// ADC channels for sensors
+#define SENSOR_1 0
+#define SENSOR_2 1
+#define SENSOR_3 2				// Middle sensor
+#define SENSOR_4 3
+#define SENSOR_5 4
+
+#define SETPOINT 255				// Setpoint for the middle sensor (ideally 255)
+
+// Function prototypes
+void initADC();					// Initialize ADC
+int8_t readADC(uint8_t channel);	// Read value from ADC channel
+void initMotors();				// Initialize motor outputs
+void setMotorSpeed(int8_t leftSpeed, int8_t rightSpeed); // Set motor speeds
+int8_t calculatePID(int8_t error); // Calculate PID correction
+
+int main() {
+	initADC();					// Initialize ADC
+	initMotors();				// Initialize motors
+
+	int8_t prev_error = 0;		// Previous error for PID
+	int8_t integral = 0;		// Integral part of PID
+
+	while (1) {
+		int8_t sensorValues[SENSOR_COUNT]; // Array to hold sensor values
+		sensorValues[0] = readADC(SENSOR_1); // Read sensor 1
+		sensorValues[1] = readADC(SENSOR_2); // Read sensor 2
+		sensorValues[2] = readADC(SENSOR_3); // Read sensor 3 (middle)
+		sensorValues[3] = readADC(SENSOR_4); // Read sensor 4
+		sensorValues[4] = readADC(SENSOR_5); // Read sensor 5
+
+		int8_t error = SETPOINT - sensorValues[2]; // Calculate error
+
+		int8_t pid = calculatePID(error); // Calculate PID value
+
+		int8_t leftMotorSpeed = 127 + pid; // Adjust left motor speed
+		int8_t rightMotorSpeed = 127 - pid; // Adjust right motor speed
+
+		setMotorSpeed(leftMotorSpeed, rightMotorSpeed); // Set motor speeds
+
+		_delay_ms(100);			// Delay for stability
+	}
+
+	return 0;
+}
+
+void initADC() {
+	ADMUX = (1 << REFS0);			// Set reference voltage to AVcc
+
+	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1); // Enable ADC with prescaler of 64
+}
+
+int8_t readADC(uint8_t channel) {
+	ADMUX = (ADMUX & 0xF8) | (channel & 0x07); // Select the correct ADC channel
+
+	ADCSRA |= (1 << ADSC);			// Start ADC conversion
+
+	while (ADCSRA & (1 << ADSC));	// Wait for conversion to complete
+
+	return (int8_t)(ADC >> 2);		// Return 8-bit result
+}
+
+void initMotors() {
+	DDRD |= (1 << PD5) | (1 << PD6); // Set PD5 and PD6 as output for motor control
+}
+
+void setMotorSpeed(int8_t leftSpeed, int8_t rightSpeed) {
+	OCR0A = leftSpeed;				// Set left motor speed (connected to PD6)
+	OCR0B = rightSpeed;				// Set right motor speed (connected to PD5)
+}
+
+int8_t calculatePID(int8_t error) {
+	static int8_t prev_error = 0;	// Previous error for derivative
+	static int8_t integral = 0;		// Integral accumulator
+
+	integral += error;				// Calculate integral
+
+	int8_t derivative = error - prev_error; // Calculate derivative
+
+	int8_t pid_output = (Kp * error) + (Ki * integral) + (Kd * derivative); // PID output
+
+	prev_error = error;				// Store current error as previous error
+
+	return pid_output;				// Return PID correction value
+}
--- README.md
+++ README.md
@@ -4,7 +4,8 @@ S2 2024
 [NOTE ON COLLARBORATION](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/about-pull-requests)
 
 ## ATMega328P Microcontroller
-[ATMega328P Datasheet](https://components101.com/microcontrollers/atmega328p-pinout-features-datasheet)
+[ATMega328P Datasheet](https://components101.com/microcontrollers/atmega328p-pinout-features-datasheet)  
+[Pinout with ADC pins explained](https://www.learningaboutelectronics.com/Articles/Atmega328-pinout.php#:~:text=Since%20the%20ADC%20for%20the%20Atmega328%20is%20a,value%3B%20thus%2C%20it%20is%20its%20digital%20correspondent%20value.)
 
 ## Downloading the bootloader (using Arduino IDE)
 [Arduino IDE](https://docs.arduino.cc/software/ide/)    <br>
@@ -13,7 +14,8 @@ S2 2024
 
 ## Programming
 [Arduino Programming](https://docs.arduino.cc/programming/)  
-[MiniCore Tips](https://github.com/MCUdude/MiniCore#how-to-install)
+[MiniCore Tips](https://github.com/MCUdude/MiniCore#how-to-install)  
+[Embedded C](https://www.learningaboutelectronics.com/EmbeddedC/)  
 
 ### Pin macros
 // Use PIN_PB5 macro to refer to pin PB5 (Arduino pin 13)  
@@ -24,9 +26,11 @@ digitalWrite(13, HIGH);
 
 ### Other things of note with MiniCore
 Printf support  
-EEPROM option (Electrically Erasable Programmable Read-Only Memory)
-BOD option
-
+EEPROM option (Electrically Erasable Programmable Read-Only Memory)  
+BOD option  
+[ADC pins explained](https://www.hackster.io/yeshvanth_muniraj/analog-to-digital-converter-module-of-atmega328p-e255ea#:~:text=Features%20of%20the%20ADC%20in%20ATmega328P:%20The#:~:text=Features%20of%20the%20ADC%20in%20ATmega328P:%20The)  
+[More on ADCs](https://www.tspi.at/2021/07/15/atmegaavradc.html#:~:text=The%20ATMega328p%20and%20the%20ATMega2560%20offers)  
+[Using the ATmega328P ADC Module](https://ece-classes.usc.edu/ee459/library/documents/ADC.pdf#:~:text=Using%20the%20Atmel%20ATmega328P%20Analog%20to)  
 
 ### Example Code
 ![image](https://github.com/user-attachments/assets/1dcd34ce-a26a-4fc3-a142-9096b09b8416)
