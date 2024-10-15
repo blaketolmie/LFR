6c7fbf5047198dcc4167dbbdbed2ba73406c59f6
Blake Tolmie <166079605+blaketolmie@users.noreply.github.com>
2024-10-16 03:18:48 +1300

    WORKING VERSION - 2 Sensors

--- LFR_BANG_BANG/LFR_BANG_BANG.ino
+++ LFR_BANG_BANG/LFR_BANG_BANG.ino
@@ -3,52 +3,57 @@
 #include "LEDs.h"
 #include "Sensors.h"
 /* Hold down reset switch to stop motors */ 
+/* If lights are flickering must manually start motors */
 
 // Motor values
-int16_t threshold = 100;        // Threshold for bang bang controller
-int16_t speed_up = 150;         // Motor speed fast
-int16_t slow_down = 100;        // Motor speed slow
-int16_t forward = 150;          // Motor speed when forward
+int16_t threshold = 230;        // Threshold for bang bang controller 200
+int16_t speed_up = 70;         // Motor speed fast, 55 without weights, 80 good for rectangle with weights, 70 perfect for complex track
+
+int16_t slow_down = 0;        // Motor speed slow
+int16_t forward = 30;          // Motor speed when forward
+
+int16_t left_speed = 65; // original 30 then 60
+int16_t right_speed = 65;
+
+// testing
+// int16_t threshold1 = 230;        // Threshold for bang bang controller
+// int16_t threshold5 = 230;
+
 
 void setup() {
-    Serial.begin(9600);
+    // Serial.begin(9600);
     initStatLEDs();
     initMotors();
     initSensors();
 }
 
 void loop() {
+    turnOnLED(LED_1);
     int16_t sensor_1 = analogRead(SENSOR_1);
-    int16_t sensor_2 = analogRead(SENSOR_2);
-    int16_t sensor_3 = analogRead(SENSOR_3);
-    int16_t sensor_4 = analogRead(SENSOR_4);
-    // If adding a 5th sensor make it so then middle sensor just means it goes straight
-
-    // Convert ADC values to digital values. Group the sensors into left and right
-    int16_t leftSensors = (sensor_1 < threshold) || (sensor_2 < threshold);
-    int16_t rightSensors = (sensor_3 < threshold) || (sensor_4 < threshold);
-
-    // Bang bang controller. Reminder that setMotorSpeed(int16_t leftSpeed, int16_t rightSpeed)
-    if (leftSensors && !rightSensors) {
+    int16_t sensor_5 = analogRead(SENSOR_5);
+    
+    if (sensor_1 < threshold) {  // when on line
         // If the left group sense the line, turn left by increasing speed of right motor etc
-        setMotorSpeed(slow_down,speed_up);
-        turnOnLED(LED_1);
-        turnOffLED(LED_2);
-        turnOffLED(LED_3);
-        
-    } else if (rightSensors && !leftSensors) {
+        left_speed = speed_up;
+        right_speed = slow_down;
+    } else if (sensor_5 < threshold) {
         // If the right group sense the line, turn right by increasing speed of left motor etc
-        setMotorSpeed(speed_up,slow_down);
-        turnOnLED(LED_2);
-        turnOffLED(LED_1);
-        turnOffLED(LED_3);
+        left_speed = slow_down;
+        right_speed = speed_up;
+    }
+    setMotorSpeed(left_speed,right_speed);
+    // setMotorSpeed(0,speed_up);
+
+
+    if (sensor_1 < threshold) {
+      turnOnLED(LED_2);
     } else {
-        // Else move forward
-        setMotorSpeed(forward,forward);
-        turnOnLED(LED_3);
-        turnOffLED(LED_1);
-        turnOffLED(LED_2);
+      turnOffLED(LED_2);
+    }
+    if (sensor_5 < threshold) {
+      turnOnLED(LED_3);
+    } else {
+      turnOffLED(LED_3);
     }
 
-    delay(500);     // Delay for 0.5 seconds (for stability) - reduce to 0.1 seconds after removing serial communication println's
 }
